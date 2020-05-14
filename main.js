9965f09babda350d76b568bb0bd8060d6cc10738
mlgmontage <kettleofrassel1@gmail.com>
2020-05-14 04:18:12 -0500

    adding operations

--- main.js
+++ main.js
@@ -1,28 +1,56 @@
 // generators
 function generateNumber() {
+  // only positive numbers
   const topValue = 10;
-  const random = parseInt(Math.random() * topValue);
+  const random = parseInt(Math.random() * topValue + 1);
   return random;
+}
+
+// generate operation
+function generateOperation() {
+  // opearations
+  const operations = ["+", "-", "*", "/"];
+  const picker = parseInt(Math.random() * operations.length);
+  return operations[picker];
 }
 
 // board variables
 let wins = 0;
 let losses = 0;
 
+// game variables
+let operandA;
+let operandB;
+let operation;
+
+// computing answer
+function compute(operandA, operation, operandB) {
+  if (operation === "+") return operandA + operandB;
+  if (operation === "-") return operandA - operandB;
+  if (operation === "*") return operandA * operandB;
+  if (operation === "/") return (operandA / operandB).toFixed(2);
+}
+
 // DOM elements
 const a = document.getElementById("a");
 const b = document.getElementById("b");
+const operationElem = document.getElementById("operation");
 const assertButton = document.getElementById("btn");
 const userInput = document.getElementById("userInput");
 const winsElement = document.getElementById("wins");
 const lossesElement = document.getElementById("losses");
 
 function newGame() {
-  const operandA = generateNumber();
-  const operandB = generateNumber();
+  operandA = generateNumber();
+  operandB = generateNumber();
+  operation = generateOperation();
 
+  // board
   a.innerText = operandA;
   b.innerText = operandB;
+  operationElem.innerText = operation;
+
+  // board
   winsElement.innerText = wins;
   lossesElement.innerText = losses;
 }
@@ -30,17 +58,21 @@ function newGame() {
 newGame();
 
 assertButton.addEventListener("click", () => {
-  const operandA = parseInt(a.innerText);
-  const operandB = parseInt(b.innerText);
-  const userAnswer = parseInt(userInput.value);
-  const answer = operandA + operandB;
-  if (userAnswer === answer) {
+  const userAnswer = userInput.value;
+
+  // computing correct answer
+  const answer = compute(operandA, operation, operandB);
+
+  // assertion
+  if (userAnswer == answer) {
     wins++;
     alert("congrats");
   } else {
     losses++;
-    alert(`correct answer is ${answer}`);
+    alert(`correct answer is "${answer}", your answer "${userAnswer}"`);
   }
+
+  // reset
   userInput.value = "";
   newGame();
 });
