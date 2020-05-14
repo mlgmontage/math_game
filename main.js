216b1a24076aa7053cc0cecb7ec20dc6710fb5e8
mlgmontage <kettleofrassel1@gmail.com>
2020-05-14 02:09:40 -0500

    first version

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 function generateNumber() {
-  const random = parseInt(Math.random() * 100);
+  const random = parseInt(Math.random() * 10);
   return random;
+}
+
+function assert(output, exceptedOutput) {
+  return output === exceptedOutput ? true : false;
 }
 
 const a = document.getElementById("a");
 const b = document.getElementById("b");
+const assertButton = document.getElementById("btn");
 
-const result = document.getElementById("userInput");
+const userInput = document.getElementById("userInput");
 
-function setup() {
-  a.innerText = generateNumber();
-  b.innerText = generateNumber();
+function newGame() {
+  const operandA = generateNumber();
+  const operandB = generateNumber();
+
+  a.innerText = operandA;
+  b.innerText = operandB;
 }
 
-setup();
+newGame();
+
+assertButton.addEventListener("click", () => {
+  const operandA = parseInt(a.innerText);
+  const operandB = parseInt(b.innerText);
+  const userAnswer = parseInt(userInput.value);
+  const answer = operandA + operandB;
+  if (assert(userAnswer, answer)) {
+    alert("congrats");
+  } else {
+    alert(`correct answer is ${answer}`);
+  }
+  userInput.value = "";
+  newGame();
+});
